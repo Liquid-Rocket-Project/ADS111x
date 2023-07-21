c9db95f95730c75e603cadaa248be85d70917956
nfan17 <nicolasfan51@gmail.com>
2023-07-21 00:52:42 -0700

    nf-ads111x-f7/api, test reading diff channels

--- ads111x.c
+++ ads111x.c
@@ -6,25 +6,72 @@
 
 #include "ads111x.h"
 
-static uint8_t config_default[2] = {
-		ADS_CONFIG_UPPER_DEFAULT,
-		ADS_CONFIG_LOWER_DEFAULT
-};
-
 static uint8_t rxdata[2] = {};
 
-void ADS_Init(uint8_t DAddress) {
-	I2C2_Init();
-	I2C2_SetDevice(DAddress);
+static uint8_t configBuffer[2] = {};
+
+
+static inline void Get_Configs(ADS111x *adc, uint8_t channel, uint8_t *configsBuff) {
+	/* You can edit this function along with the base config
+	 * macros to adjust what gets set each time
+	 */
+	configsBuff[0] = ADS_CONFIG_LOWER_BASE | adc->datarate;
+	configsBuff[1] = ADS_CONFIG_UPPER_BASE | ADS_CONFIG_OS | adc->mode
+				| adc->gain | channel;
+}
+
+extern ADS111x ADS_Default_Struct(uint8_t DAddress) {
+	ADS111x ret = {
+		ADS_PGA_2_048V,
+		ADS_CONFIG_MODE,
+		ADS_DR_128_SPS,
+		DAddress,
+	};
+	return ret;
 
 }
 
-uint16_t ADS_SampleChannel(uint8_t channel) {
+void ADS_InitSerial(ADS111x *adc) {
+	// Initialize i2c
+	adc->init();
 
-	I2C2_MemWrite(ADS_REG_CONFIG_PTR, config_default, 2, 1000);
+}
 
-	I2C2_MemRead(ADS_REG_CONVERSION_PTR, rxdata, 2, 1000);
+uint16_t ADS_SampleChannel(ADS111x *adc, uint8_t channel) {
+
+	// Store configs in buffer to send
+	Get_Configs(adc, channel, configBuffer);
+
+	// Set target adc
+	adc->selectDevice(adc->DAddress);
+
+	// Configure and start conversion
+	adc->memWrite(ADS_REG_CONFIG_PTR, configBuffer, ADS_CONFIG_BUFFER_SIZE, 1000);
+
+	// Read last conversion
+	adc->memRead(ADS_REG_CONVERSION_PTR, rxdata, ADS_CONFIG_BUFFER_SIZE, 1000);
 
 	return (uint16_t) ((rxdata[1] << 8) | rxdata[0]);
 
+}
+
+void ADS_StartConversion(ADS111x *adc, uint8_t channel) {
+	// Store configs in buffer to send
+	Get_Configs(adc, channel, configBuffer);
+
+	// Set target adc
+	adc->selectDevice(adc->DAddress);
+
+	// Configure and start conversion
+	adc->memWrite(ADS_REG_CONFIG_PTR, configBuffer, ADS_CONFIG_BUFFER_SIZE, 1000);
+}
+
+uint16_t ADS_ReadLastConversion(ADS111x *adc) {
+	// Set target adc
+	adc->selectDevice(adc->DAddress);
+
+	// Read last conversion
+	adc->memRead(ADS_REG_CONVERSION_PTR, rxdata, ADS_CONFIG_BUFFER_SIZE, 1000);
+
+	return (uint16_t) ((rxdata[1] << 8) | rxdata[0]);
 }
